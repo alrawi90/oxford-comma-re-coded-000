de0538cf16ae8200dc1dfb02935d2f169dbf7ac4
ALI ALRAWI <ali.developer90@gmail.com>
2016-10-07 14:12:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
+  line=""
+  c=1
+  len=array.size
+  if array.size==1
+    return array[0]
+  elsif  array.size==2
+    return  array[0]+" and "+array[1]
+  elsif  array.size==3
+    return  array[0]+", "+array[1]+", and "+array[2]
+  else
+    array.each do |e|
+      if c==len
+       line=line + "and " + e
+     else
+       line=line + e + ", "
+     end
+     c +=1
+    end
+    return line
 
 end
+
+end
+puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
